bd7ff78ed21414008af01f6f7a684afd91ace833
jk <generaljking@163.com>
2017-06-11 22:33:42 +0800

    add Library

---
+++ MyLibraryTest/src/main/java/com/jk/mylibrarytest/MainActivity_Library.java
@@ -0,0 +1,18 @@
+package com.jk.mylibrarytest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/11.
+ */
+
+public class MainActivity_Library extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- app/src/main/java/com/jk/jkdaily/MainActivity.java
+++ app/src/main/java/com/jk/jkdaily/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jk.jkdaily;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.jk.mylibrarytest.MainActivity_Library;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv_test_library).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity_Library.class));
+            }
+        });
     }
 }
